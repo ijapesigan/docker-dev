4997541b358a2c9af8d1b3f15e4830c324f4fe9a
ijapesigan <ijapesigan@gmail.com>
2023-11-23 16:07:55 +0000

    Automated build.

--- scripts/tinytex.sh
+++ scripts/tinytex.sh
@@ -2,11 +2,21 @@
 
 set -e
 
+# tinytex
+install2.r --error --skipinstalled -n -1 \
+    tinytex
+
 # try() to avoid errors on arm64
 # https://github.com/rstudio/tinytex-releases/issues/37
 
+# install tinytex package
 R -e "try(tinytex::install_tinytex( \
-    bundle = 'TinyTeX',         \
+    bundle = 'TinyTeX',       \
     force = TRUE,               \
     dir =  '/opt/TinyTeX'       \
 ))"
+
+# Check tinytex
+echo -e "Check tinytex...\n"
+R -e "tinytex::is_tinytex()"
+echo -e "\nInstall tinytex, done!"
