ccd19a0e4e9df50ad5ba78eb6cca3bfa5b248a13
ijapesigan <ijapesigan@gmail.com>
2023-11-23 16:05:10 +0000

    Automated build.

--- scripts/tinytex.sh
+++ scripts/tinytex.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-R -e "tinytex::install_tinytex( \
+# try() to avoid errors on arm64
+# https://github.com/rstudio/tinytex-releases/issues/37
+
+R -e "try(tinytex::install_tinytex( \
     bundle = 'TinyTeX',         \
     force = TRUE,               \
     dir =  '/opt/TinyTeX'       \
-)"
+))"
